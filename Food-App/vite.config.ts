cb74c57b087680bf9c6f6c7cd1027fe32e0874d7
anupkumarmridha <anupkumarmridha.net@gmail.com>
2024-09-26 16:10:52 +0530

    changes for vite config

--- Food-App/vite.config.ts
+++ Food-App/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/ˀReact-Mini-Project/Food-App/dist/"
+  base:"/React-Mini-Project/Food-App/dist/"
 })
